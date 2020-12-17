42bdda5f3a81d85e161e94aa854a37633d6f26e5
bertil291utn <bertilt4@gmail.com>
2020-12-17 18:16:18 -0500

    Add predictor class - Add as main class to call on index file

---
+++ objects/predictor.js
@@ -0,0 +1,23 @@
+class Predictor {
+  constructor(car, day, hour) {
+    this._car = car;
+    this._day = day;
+    this._hour = hour;
+  }
+
+  get isRestrictedDay() {
+    if (this._day == 6 || this._day == 0) return false;
+    const restrictions = {
+      1: [1, 2],
+      2: [3, 4],
+      3: [5, 6],
+      4: [7, 8],
+      5: [9, 0]
+    }
+    return restrictions[this._day].includes(this._car.plate)
+  }
+
+  get isRestrictedHour() {
+    return (this._hour >= '07:00' && this._hour <= '09:30') || (this._hour >= '16:00' && this._hour <= '19:30');
+  }
+}
