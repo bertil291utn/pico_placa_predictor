2fab23a6c429e5127b1f189f71de7b610332595f
bertil291utn <bertilt4@gmail.com>
2020-12-17 17:54:02 -0500

    Add test clases for isRestrictedHourFunctions
    - Only two test cases

--- objects/predict.test.js
+++ objects/predict.test.js
@@ -1,14 +1,27 @@
 const { isRestrictedDay, isRestrictedHour } = require('./logicfunctions');
 
-test('should return true after pass a restricted last plate number in a specific day ', () => {
-  expect(isRestrictedDay(5, 9)).toBeTruthy();
+describe('#isRestrictedHour', () => {
+  test('should return true after pass a restricted last plate number in a specific day ', () => {
+    expect(isRestrictedDay(5, 9)).toBeTruthy();
+  });
+
+  test('should return false when pass a non restricted day ', () => {
+    expect(isRestrictedDay(6, 9)).toBeFalsy();
+  });
+
+  test('should return false when pass non restricted last plate number in a specific day ', () => {
+    expect(isRestrictedDay(4, 0)).toBeFalsy();
+  });
 });
 
-test('should return false when pass a non restricted day ', () => {
-  expect(isRestrictedDay(6, 9)).toBeFalsy();
-});
 
-test('should return false when pass non restricted last plate number in a specific day ', () => {
-  expect(isRestrictedDay(4, 0)).toBeFalsy();
-});
+describe('#isRestrictedHour', () => {
 
+  test('should return true after pass a restricted hour', () => {
+    expect(isRestrictedHour('07:00')).toBeTruthy();
+  });
+
+  test('should return false after pass a non restricted hour', () => {
+    expect(isRestrictedHour('11:00')).toBeFalsy();
+  });
+});
